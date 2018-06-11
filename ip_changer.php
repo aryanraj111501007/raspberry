ffa5e7e88062f50ed2216deb3a272a9b710b993a
aryanraj111501007 <111501007@smail.iitpkd.ac.in>
2018-06-11 14:20:57 +0530

    aDDED

--- ip_changer.php
+++ ip_changer.php
@@ -27,7 +27,7 @@
     echo "gi=".$gi;
     echo "gain=".$Gain;
     $myfile = fopen("/home/pi/Desktop/ar.txt", "w") or die("could,nt open\n");
-    $myfile1=fopen("ar.txt") or die("could,nt open ar\n");
+    $myfile1=fopen("ar.txt" ,"w") or die("could,nt open ar\n");
     
     for($i=0;$i<$count;$i++)
     {
